4b4e9c52a3cc403702f5ecb06281c2f054671db6
bcoka <bojan.coka@outlook.com>
2014-01-12 20:15:50 +0100

    Remove extraneous dependecies

--- src/cvLibs.h
+++ src/cvLibs.h
@@ -1,10 +1,7 @@
 #pragma once
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 using cv::Mat;
