60c561f526b3d95633fef761fb6ebf9f97d71b78
bcoka <bojan.coka@outlook.com>
2014-01-13 03:28:27 +0100

    Encapsulate mask determination

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include "valarray2d.h"
 #include "bucket.h"
 
-#define DRAW_OUTPUT
+#define DRAW_MASK
 // #define WRITE_OUTPUT
 
 void distance(double& dpq, double& Lp, double& Lq, double& ap, double& aq, double& bp, double& bq);
+void createMask(unsigned int row, unsigned int col, unsigned int bucketSize, double threshold, Mat& image, Mat& source);
 
 int main(int argc, char** argv)
 {
@@ -28,14 +29,14 @@ int main(int argc, char** argv)
 
 	// parameters
 	const double threshold{ 8.0 };
-#ifdef DRAW_OUTPUT
+#ifdef DRAW_MASK
 	const unsigned int bucketSize{ 256 };
 #else
 	const unsigned int bucketSize{ 16 };
 #endif
 
 	// for each pixel of image1
-#ifdef DRAW_OUTPUT
+#ifdef DRAW_MASK
 	for (unsigned int row{ bucketSize / 2 }; row < height1; row += bucketSize)
 	{
 		for (unsigned int col{ bucketSize / 2 }; col < width1; col += bucketSize)
@@ -47,67 +48,7 @@ int main(int argc, char** argv)
 		{
 #endif
 			// determine ABW shape
-			
-			// construct ABW bucket
-			bucket <double> testWindow{ row, col, bucketSize };
-
-			// crop bucket
-			if (testWindow.get_x1() < 0) { testWindow.set_x1(0); }
-			if (testWindow.get_y1() < 0) { testWindow.set_y1(0); }
-			if (testWindow.get_x2() > width1) { testWindow.set_x2(width1); }
-			if (testWindow.get_y2() > height1) { testWindow.set_y2(height1); }
-
-			int leftBorder{ testWindow.get_x1() };
-			int topBorder{ testWindow.get_y1() };
-			int rightBorder{ testWindow.get_x2() };
-			int bottomBorder{ testWindow.get_y2() };
-
-			int rl{ leftBorder }; // temporary
-			int rt{ topBorder }; // temporary
-
-			// evaluate center bucket pixel
-			double Lp{};
-			double ap{};
-			double bp{};
-			readLabPixel(image1_lab, Lp, ap, bp, row, col);
-
-			// prepare variables for other bucket pixels
-			double Lq{};
-			double aq{};
-			double bq{};
-
-			double dpq{};
-
-			// for each pixel of bucket
-#ifdef DRAW_OUTPUT
-			Mat image1_copy{ image1.clone() };
-#endif
-			while (leftBorder < rightBorder)
-			{
-				topBorder = rt;
-				while (topBorder < bottomBorder)
-				{
-					readLabPixel(image1_lab, Lq, aq, bq, topBorder, leftBorder);
-					distance(dpq, Lp, Lq, ap, aq, bp, bq);
-					if (dpq < threshold) { testWindow.set_value(leftBorder - rl, topBorder - rt, 255); }
-#ifdef DRAW_OUTPUT
-					image1_copy.at<Vec3b>(topBorder, leftBorder).val[0] = testWindow.get_value(leftBorder - rl, topBorder - rt);
-					image1_copy.at<Vec3b>(topBorder, leftBorder).val[1] = testWindow.get_value(leftBorder - rl, topBorder - rt);
-					image1_copy.at<Vec3b>(topBorder, leftBorder).val[2] = testWindow.get_value(leftBorder - rl, topBorder - rt);
-#endif
-					topBorder++;
-				}
-				leftBorder++;
-			}
-#ifdef DRAW_OUTPUT
-			imshow("Output", image1_copy);
-			waitKey(1000);
-#endif
-#ifdef WRITE_OUTPUT
-			stringstream filename{};
-			filename << "image1_abw_" << j << "_" << i << "_size" << bucketSize << "_t" << threshold << ".png";
-			imwrite(filename.str().c_str(), image1_copy);
-#endif
+			createMask( row, col, bucketSize, threshold, image1_lab, image1 );
 		}
 	}
 
@@ -121,4 +62,68 @@ void distance(double& dpq, double& Lp, double& Lq, double& ap, double& aq, doubl
 	dpq += pow((ap - aq), 2);
 	dpq += pow((bp - bq), 2);
 	dpq = sqrt(dpq);
+}
+
+void createMask(unsigned int row, unsigned int col, unsigned int bucketSize, double threshold, Mat& image, Mat& source)
+{
+// construct ABW bucket
+bucket <double> testWindow{ row, col, bucketSize };
+
+// crop bucket
+if (testWindow.get_x1() < 0) { testWindow.set_x1(0); }
+if (testWindow.get_y1() < 0) { testWindow.set_y1(0); }
+if (testWindow.get_x2() > image.cols) { testWindow.set_x2(image.cols); }
+if (testWindow.get_y2() > image.rows) { testWindow.set_y2(image.rows); }
+
+int leftBorder{ testWindow.get_x1() };
+int topBorder{ testWindow.get_y1() };
+int rightBorder{ testWindow.get_x2() };
+int bottomBorder{ testWindow.get_y2() };
+
+int rl{ leftBorder }; // temporary
+int rt{ topBorder }; // temporary
+
+// evaluate center bucket pixel
+double Lp{};
+double ap{};
+double bp{};
+readLabPixel(image, Lp, ap, bp, row, col);
+
+// prepare variables for other bucket pixels
+double Lq{};
+double aq{};
+double bq{};
+
+double dpq{};
+
+// for each pixel of bucket
+#ifdef DRAW_MASK
+Mat image_copy{ source.clone() };
+#endif
+while (leftBorder < rightBorder)
+{
+	topBorder = rt;
+	while (topBorder < bottomBorder)
+	{
+		readLabPixel(image, Lq, aq, bq, topBorder, leftBorder);
+		distance(dpq, Lp, Lq, ap, aq, bp, bq);
+		if (dpq < threshold) { testWindow.set_value(leftBorder - rl, topBorder - rt, 255); }
+#ifdef DRAW_MASK
+		image_copy.at<Vec3b>(topBorder, leftBorder).val[0] = testWindow.get_value(leftBorder - rl, topBorder - rt);
+		image_copy.at<Vec3b>(topBorder, leftBorder).val[1] = testWindow.get_value(leftBorder - rl, topBorder - rt);
+		image_copy.at<Vec3b>(topBorder, leftBorder).val[2] = testWindow.get_value(leftBorder - rl, topBorder - rt);
+#endif
+		topBorder++;
+	}
+	leftBorder++;
+}
+#ifdef DRAW_MASK
+imshow("Output", image_copy);
+waitKey(1000);
+#endif
+#ifdef WRITE_OUTPUT
+stringstream filename{};
+filename << "image1_abw_" << j << "_" << i << "_size" << bucketSize << "_t" << threshold << ".png";
+imwrite(filename.str().c_str(), image1_copy);
+#endif
 }
